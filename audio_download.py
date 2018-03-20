3ea7f97c15ac35edc17bca850ffb901000566199
Kai Fleischman <KaiFleischmans@gmail.com>
2018-03-20 15:27:50 -0500

    Modified audio_download to read from a list of URLs and downloand MP3 files one by one.

--- audio_download.py
+++ audio_download.py
@@ -1,6 +1,9 @@
 import pafy
 
-url = "https://www.youtube.com/watch?v=gE68v9QotNA" #Audio for Soulja Boy - Crank That
-video = pafy.new(url)
-best_audio = video.getbestaudio()
-current_file = best_audio.download("CurrentSong.mp3")
+input_file = open("SongURLs.txt")
+urls = input_file.readlines()
+for url in urls:
+	url = url.strip()
+	video = pafy.new(url)
+	best_audio = video.getbestaudio()
+	current_file = best_audio.download("\songs")
