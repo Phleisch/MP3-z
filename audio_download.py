49e607184ac729df0bf338356e5c6f6068e8eb13
Kai Fleischman <KaiFleischmans@gmail.com>
2018-03-20 15:59:48 -0500

    Made slight changes, such as naming output mp3 files after the name of the song.

--- audio_download.py
+++ audio_download.py
@@ -1,9 +1,17 @@
+import re
 import pafy
 
-input_file = open("SongURLs.txt")
-urls = input_file.readlines()
-for url in urls:
-	url = url.strip()
-	video = pafy.new(url)
-	best_audio = video.getbestaudio()
-	current_file = best_audio.download("\songs")
+def download_audio(urls):
+	regex = '[ ]'
+	input_file = urls
+	urls = input_file.readlines()
+	for url in urls:
+		url = url.strip()
+		parts = url.split("#")
+		print("\nCurrently downloading %s..."%parts[1])
+		video = pafy.new(parts[0])
+		best_audio = video.getbestaudio()
+		title = parts[1].title()
+		title = re.sub(regex, '', title)
+		file_save = "songs/" + title + ".mp3"
+		current_file = best_audio.download(file_save)
