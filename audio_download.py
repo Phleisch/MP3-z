919ca5f5fa5e69d6c029b2cf7a996198a12adc8a
Kai Fleischman <KaiFleischmans@gmail.com>
2018-03-20 14:25:18 -0500

    Successfully downloaded an mp3 file from a YouTube video link.

--- audio_download.py
+++ audio_download.py
@@ -2,5 +2,5 @@ import pafy
 
 url = "https://www.youtube.com/watch?v=gE68v9QotNA" #Audio for Soulja Boy - Crank That
 video = pafy.new(url)
-audiostreams = video.audiostreams
-audiostreams[0].download()
+best_audio = video.getbestaudio()
+current_file = best_audio.download("CurrentSong.mp3")
