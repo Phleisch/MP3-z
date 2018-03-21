4049940f11ac3a482437fc85c701c92afbc00489
Kai Fleischman <KaiFleischmans@gmail.com>
2018-03-21 13:52:22 -0500

    Minor changes to fix output song file names.

--- audio_download.py
+++ audio_download.py
@@ -1,17 +1,24 @@
 import re
+import string
 import pafy
 
+def title_except(s, exceptions):
+    word_list = re.split(' ', s)       # re.split behaves as expected
+    final = [word_list[0].capitalize()]
+    for word in word_list[1:]:
+        final.append(word if word in exceptions else word.capitalize())
+    return " ".join(final)
+
 def download_audio(urls):
-	regex = '[ ]'
+	articles = ['a', 'an', 'of', 'the', 'is']
 	input_file = urls
 	urls = input_file.readlines()
 	for url in urls:
-		url = url.strip()
+		url = url.strip('\n')
 		parts = url.split('#')
 		print('\nCurrently downloading %s...'%parts[1])
 		video = pafy.new(parts[0])
 		best_audio = video.getbestaudio()
-		title = parts[1].title()
-		title = re.sub(regex, '', title)
+		title = title_except(parts[1], articles)
 		file_save = 'songs/' + title + '.mp3'
 		current_file = best_audio.download(file_save)
--- retrieve_urls.py
+++ retrieve_urls.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 def get_urls(song_list):
 	urls = open('SongURLs.txt', 'w')
 	songs = song_list.readlines()
-	regex = '[^A-Za-z0-9 ]'
+	regex = '[^A-Za-z0-9\'\. ]'
 	search_const = 'https://www.youtube.com/results?search_query='
 
 	for song in songs:
@@ -23,7 +23,9 @@ def get_urls(song_list):
 		soup = BeautifulSoup(html, 'html5lib')
 		videos_info = soup.findAll(attrs={'class':'yt-uix-tile-link'})
 		first_video_info = videos_info[0]
-		first_url = 'https://www.youtube.com' + first_video_info['href'] + '#' + unchanged
+		parts = first_video_info['href'].split('=')
+		parts[1] = urllib.parse.quote(parts[1])
+		first_url = 'https://www.youtube.com' + parts[0] + "=" + parts[1] + '#' + unchanged
 		urls.write('%s\n'%first_url)
 		song_list.write('%s\n'%('#' + unchanged))
 	urls.close()
