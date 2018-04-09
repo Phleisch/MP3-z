e099f205e7d892d3403c6fcc57db9fc3fc94332d
Kai Fleischman <KaiFleischmans@gmail.com>
2018-04-09 01:45:42 -0500

    Added a function to get the URL for just one song as a String parameter, not a file

--- retrieve_urls.py
+++ retrieve_urls.py
@@ -30,3 +30,25 @@ def get_urls(song_list):
 		song_list.write('%s\n'%('#' + unchanged))
 	urls.close()
 	song_list.close()
+
+def get_url(song):
+	regex = '[^A-Za-z0-9\'\. ]'
+	search_const = 'https://www.youtube.com/results?search_query='
+	if '#' in song:
+		continue
+	song = song.strip('\n')
+	unchanged = song
+	song = re.sub(regex, '', song)
+	song = song + ' audio'
+	song = urllib.parse.quote(song)
+	query = search_const + song
+	#print(query)
+	response = urllib.request.urlopen(query)
+	html = response.read()
+	soup = BeautifulSoup(html, 'html5lib')
+	videos_info = soup.findAll(attrs={'class':'yt-uix-tile-link'})
+	first_video_info = videos_info[0]
+	parts = first_video_info['href'].split('=')
+	parts[1] = urllib.parse.quote(parts[1])
+	first_url = 'https://www.youtube.com' + parts[0] + "=" + parts[1] + '#' + unchanged
+	return first_url
